2e562ba60cdff73e3be94b4c315977945ecbed58
Sanket176 <sanketrinayat17@gmail.com>
2024-06-09 14:32:22 +0530

    changed hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = ()=>{
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Sanket</h1>
             <p className={styles.description}>I'm a Frontend Developer, with 2 years of experience using JS and ReactJS. 
-                Reach out if you want to live more!
+                Reach out if you want to know more!
             </p>
             <a href="mailto:sanketrinayat17@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         {/* Image on the Right hand side */}
-        <img src={getImageUrl("hero/heroImage.png")} className={styles.heroImg} alt="Hero image of me"/>
+        <img src={getImageUrl("hero/PP-circle.png")} className={styles.heroImg} alt="Hero image of me"/>
         <div className={styles.topBlur} />
         <div className={styles.bottomBlur} />
     </section>;
